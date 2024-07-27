048598fc46eb9ab593b4845fe9ef8c4f462917fa
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-07-27 12:31:34 +0200

    import addwrite from node

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+import { Client, Databases } from "node-appwrite";
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
 export default async ({ req, res, log, error }) => {
+	const client = new Client();
+
+	client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
+
 	return res.send("Hey fuckers!");
 };
