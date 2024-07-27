956992ccf96ddc884a2223bced89bf2307fd5d3e
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-07-27 12:56:52 +0200

    logging out list of profiles

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 import { Client, Databases } from "node-appwrite";
 
 const PROJECT_ID = process.env.PROJECT_ID;
+const DB_ID = process.env.DB_ID;
+const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES;
 
 export default async ({ req, res, log, error }) => {
 	const client = new Client();
 
 	client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
+
+	const db = new Databases(client);
+
+	if (req.method == "get") {
+		const res = await db.listDocuments(DB_ID, COLLECTION_ID_PROFILES);
+
+		log(res);
+	}
 
 	return res.send(PROJECT_ID);
 };
